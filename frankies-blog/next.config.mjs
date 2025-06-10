831a46be5114ef524597afd692c4078a1ef749fb
Bwillia13x <bdrewwilliams@gmail.com>
2025-06-10 16:49:17 -0600

    fix: resolve merge conflicts with main

--- frankies-blog/next.config.mjs
+++ frankies-blog/next.config.mjs
@@ -1,9 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+import nextMdx from '@next/mdx'
 
-if (process.env.GITHUB_ACTIONS) {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
-  if (repo) nextConfig.basePath = `/${repo}`;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 }
 
-export default nextConfig;
+if (process.env.GITHUB_ACTIONS) {
+  const repo = process.env.GITHUB_REPOSITORY.split('/')[1]
+  if (repo) nextConfig.basePath = `/${repo}`
+}
+
+const withMDX = nextMdx({
+  extension: /\.mdx?$/,
+})
+
+export default withMDX(nextConfig)
