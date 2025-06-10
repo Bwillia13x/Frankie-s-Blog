4436db2583b723907750ad433ca275d80f24e9c3
Bwillia13x <bdrewwilliams@gmail.com>
2025-06-10 16:17:11 -0600

    ci: lint+test+deploy to GH Pages

--- frankies-blog/next.config.mjs
+++ frankies-blog/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // config options here
-};
+const nextConfig = { output: 'export' };
+
+if (process.env.GITHUB_ACTIONS) {
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+  if (repo) nextConfig.basePath = `/${repo}`;
+}
+
 export default nextConfig;
