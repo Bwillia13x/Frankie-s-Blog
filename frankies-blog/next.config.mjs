24eb69790f5e393ed81cbc3f8bc77fc38b5cf378
Bwillia13x <bdrewwilliams@gmail.com>
2025-06-10 16:14:30 -0600

    feat: add MDX blogging support

--- frankies-blog/next.config.mjs
+++ frankies-blog/next.config.mjs
@@ -1,5 +1,12 @@
+import nextMdx from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
-export default nextConfig;
+
+const withMDX = nextMdx({
+  extension: /\.mdx?$/,
+});
+
+export default withMDX(nextConfig);
